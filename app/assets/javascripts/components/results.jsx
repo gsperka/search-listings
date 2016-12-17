f243220242a26ad0bf038e58fd26861193e06ad7
Gary <gsperka@gmail.com>
2016-12-17 16:20:41 -0600

    add check if nil functions to views

--- app/assets/javascripts/components/results.jsx
+++ app/assets/javascripts/components/results.jsx
@@ -1,36 +1,37 @@
 var Results = React.createClass({
 
-
-	 checkNil: function(event){
-	 	console.log(event)
-	 	return (
-	 		<div>{event}</div>
-	 	)
-	 },
-
-
 	render: function() {
 	 var listings = this.props.results
+
+	function checkIfNil(val) {
+		if (val) {
+			return (
+				<span>{val}</span>
+			)
+		} else {
+			return (
+				<span>Not currently listed</span>
+			)
+		}
+	}
 
 	 var all_objects = listings.map(function(result,i){
 	 		return (
 	 			<div key={i} className="result">
 	 			  <img className="img-responsive" src={result.primary_small_image_url} />
-	 				<h3>{result.title}</h3>
-
-	 				<h3>{this.checkNil()}</h3>
-
+	 				<h3>{checkIfNil(result.title)}</h3>
+	 				
 	 				<h3>City:</h3>
-	 				<p>{result.city}</p>
+	 				<p>{checkIfNil(result.city)}</p>
 
 	 				<h3>Province:</h3>
-	 				<p>{result.province}</p>
+	 				<p>{checkIfNil(result.province)}</p>
 
 	 				<h3>Nightly From:</h3>
-	 				<p>{result.nightly_from}</p>
+	 				<p>${checkIfNil(result.nightly_from)}</p>
 
 	 				<h3>Nightly Rate:</h3>
-	 				<p>{result.nightly_rate}</p>
+	 				<p>${checkIfNil(result.nightly_rate)}</p>
 
 	 				<h5><a href={"/results/" + result.id}>See Full Listing</a></h5>
 	 			</div>
---
+++ app/assets/javascripts/components/show.jsx
@@ -0,0 +1,51 @@
+var ShowResult = React.createClass({
+
+	checkIfNil: function(val) {
+		if (val) {
+			return (
+				<span>{val}</span>
+			)
+		} else {
+			return (
+				<span>Not currently listed</span>
+			)
+		}
+	},
+
+	render: function() {
+		return (
+			<div>
+				<div className="single-listing">
+					<img src={this.props.listing.primary_small_image_url} />
+					<h1>{this.checkIfNil(this.props.listing.title)}</h1>
+				
+					<h3>Why stay here?</h3>
+					<p>{this.checkIfNil(this.props.listing.why_stay_here)}</p>
+
+					<h3>Where?</h3>
+					<p>City: {this.checkIfNil(this.props.listing.city)}</p>
+					<p>Province: {this.checkIfNil(this.props.listing.province)}</p>
+					<p>Country: {this.checkIfNil(this.props.listing.country)}</p>
+
+					<h3>Cost:</h3>
+					<p>Nightly From: ${this.checkIfNil(this.props.listing.nightly_from)}</p>
+					<p>Nightly Rate: ${this.checkIfNil(this.props.listing.nightly_rate)}</p>
+
+					<h3>Features:</h3>
+					<p>Number of Bedrooms: {this.checkIfNil(this.props.listing.num_bedrooms)}</p>
+					<p>Number of Bathrooms: {this.checkIfNil(this.props.listing.num_bathrooms)}</p>
+					<p>Sleeps Max: {this.checkIfNil(this.props.listing.sleeps_max)} (Sleeps Comfortably: {this.checkIfNil(this.props.listing.sleeps_comfortably)})</p>
+					<p>Showcased Features: {this.checkIfNil(this.props.listing.showcased_features)}</p>
+
+					<h3>Reviews:</h3>
+					<p>User Rating: {this.checkIfNil(this.props.listing.user_rating)}</p>
+					<p>Number of Reviews: {this.checkIfNil(this.props.listing.num_reviews)}</p>
+				</div>
+
+				<div className="back-btn">
+					<a href="/">Back to All Listings</a>
+				</div>
+			</div>
+		)
+	}
+});
